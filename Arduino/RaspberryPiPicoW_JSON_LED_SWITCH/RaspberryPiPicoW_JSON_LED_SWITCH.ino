efdaaa642c989303d2b4217a3ef2a80f3b2f6bac
Rune Langøy <rune.langoy@gmail.com>
2024-09-15 21:12:14 +0200

    Added source

---
+++ Arduino/RaspberryPiPicoW_JSON_LED_SWITCH/RaspberryPiPicoW_JSON_LED_SWITCH.ino
@@ -0,0 +1,39 @@
+#include "ArduinoJson.h"
+
+void setup() {
+  // Initialize the LED as an output
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// Allocate the JSON document
+JsonDocument doc;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (BOOTSEL) {
+      Serial.println(F("{\"SwitchBootSel\": 1}"));//'{ "SwitchBootSel": 1}'
+      while (BOOTSEL) {
+          delay(1);
+      }
+      Serial.println(F("{\"SwitchBootSel\": 0}"));//'{ "SwitchBootSel": 0}'
+  }
+
+  if(Serial.available())
+  {
+      String input=Serial.readString();
+      // Serial.println(input); //Debug
+      deserializeJson(doc, input);
+
+      if(doc.containsKey("userLedOn")) 
+      { long userLedStatus= doc["userLedOn"];
+        // Serial.println(userLedStatus); // Debug
+        if(userLedStatus==1)
+          digitalWrite(LED_BUILTIN, HIGH);
+        if(userLedStatus==0)
+          digitalWrite(LED_BUILTIN, LOW);
+           
+      }
+  }
+
+
+}
